MFYDev/ghost-cache-invalidation-proxy-fix

feat(webhook): Add public site URL for cache purge requests

src/index.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     app.listen(config.port, () => {
       console.log(`🚀 Server running on port ${config.port}`);
       config.debug && console.log('🐛 Debug mode enabled');
-      console.log(`🔗 Connected to Ghost CMS at ${config.ghostUrl}`);
+      console.log(`🔗 Connected to Ghost CMS at ${config.ghostInternalUrl}`);
       console.log(`📣 Webhook configured at ${config.webhook.url}`);
     });
   } catch (error: unknown) {

src/middleware.ts
@@ -15,7 +15,7 @@ export class ProxyMiddleware {
 
   private createProxy(): httpProxy {
     const proxy = httpProxy.createProxyServer({
-      target: this.config.ghostUrl,
+      target: this.config.ghostInternalUrl,
       secure: false,
       changeOrigin: true,
       selfHandleResponse: true,
@@ -190,6 +190,6 @@ img {
   }
 
   public handleRequest = (req: Request, res: Response): void => {
-    this.proxy.web(req, res, { target: this.config.ghostUrl });
+    this.proxy.web(req, res, { target: this.config.ghostInternalUrl });
   };
 } 

src/types.ts
@@ -1,5 +1,6 @@
 export interface MiddlewareConfig {
-  ghostUrl: string;
+  ghostInternalUrl: string;
+  ghostPublicUrl: string;
   port: number;
   debug: boolean;
   webhook: {

magicpages/ghost-cache-invalidation-proxy

src/index.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     app.listen(config.port, () => {
       console.log(`🚀 Server running on port ${config.port}`);
       config.debug && console.log('🐛 Debug mode enabled');
-      console.log(`🔗 Connected to Ghost CMS at ${config.ghostUrl}`);
+      console.log(`🔗 Connected to Ghost CMS at ${config.ghostUrl}`); 
       console.log(`📣 Webhook configured at ${config.webhook.url}`);
     });
   } catch (error: unknown) {

src/middleware.ts
@@ -15,7 +15,7 @@ export class ProxyMiddleware {
 
   private createProxy(): httpProxy {
     const proxy = httpProxy.createProxyServer({
-      target: this.config.ghostUrl,
+      target: this.config.ghostUrl, 
       secure: false,
       changeOrigin: true,
       selfHandleResponse: true,
@@ -190,6 +190,6 @@ img {
   }
 
   public handleRequest = (req: Request, res: Response): void => {
-    this.proxy.web(req, res, { target: this.config.ghostUrl });
+    this.proxy.web(req, res, { target: this.config.ghostUrl }); 
   };
 } 

src/types.ts
@@ -1,5 +1,6 @@
 export interface MiddlewareConfig {
-  ghostUrl: string;
+  ghostUrl: string; 
+  ghostPublicUrl?: string; // Make optional
   port: number;
   debug: boolean;
   webhook: {